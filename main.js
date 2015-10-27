3d558abe58c113e52572f1d55f7ab10a2fb02bd9
Tughi <admin@tughi.com>
2015-10-28 00:18:18 +0100

    dynamic statements

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+var fs = require('fs');
+
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('content.db');
 
 db.get('PRAGMA user_version', [], function (error, row) {
   switch (row.user_version) {
     case 0:
+      var exercises = JSON.parse(fs.readFileSync('memoria-exercises.json', 'utf8'));
+
       db.serialize(function () {
-        db.run('CREATE TABLE exercises (id INTEGER PRIMARY KEY, scope TEXT NOT NULL UNIQUE, definition TEXT NOT NULL, notes TEXT, rating INTEGER NOT NULL, updated_time INTEGER NOT NULL, practice_time INTEGER NOT NULL)');
+        db.run('CREATE TABLE exercises (id INTEGER PRIMARY KEY, created_time INTEGER NOT NULL, updated_time INTEGER NOT NULL, scope TEXT NOT NULL UNIQUE, definition TEXT NOT NULL, notes TEXT, rating INTEGER NOT NULL, practice_time INTEGER NOT NULL)');
         db.run('PRAGMA user_version = 1');
+
+        var updatedTime = new Date().getTime();
+        for (var index in exercises) {
+          var exercise = exercises[index];
+          console.log(exercise);
+          db.run(
+            'INSERT INTO exercises (scope, definition, notes, rating, practice_time, created_time, updated_time) VALUES (?, ?, ?, ?, ?, ?, ?)',
+            [exercise.scope, exercise.definition, exercise.notes, exercise.rating, exercise.practiceTime * 1000, exercise.id, updatedTime]
+          )
+        }
       });
   }
 
@@ -20,18 +34,29 @@ var app = express();
 
 app.use('/api', bodyParser.json());
 
-app.get('/api/v1/exercises', function (request, response) {
-  db.all('SELECT * FROM exercises ORDER BY id', function (error, rows) {
+app.get('/api/v1/:table', function (request, response) {
+  console.log('GET ' + request.path);
+  db.all(['SELECT * FROM ', request.params['table'], ' ORDER BY id'].join(''), function (error, rows) {
     response.json(rows);
   })
 });
 
-app.post('/api/v1/exercises', function (request, response) {
-  var exercise = request.body;
-  console.log(exercise);
+app.post('/api/v1/:table', function (request, response) {
+  console.log('POST ' + request.path);
+  var row = request.body;
+
+  var columns = [];
+  var placeholders = [];
+  var params = [];
+  for (var column in row) {
+    columns.push(column);
+    placeholders.push('?');
+    params.push(row[column]);
+  }
+
   db.run(
-    'INSERT INTO exercises (id, scope, definition, notes, rating, updated_time, practice_time) VALUES (?, ?, ?, ?, ?, ?, ?)',
-    [exercise.id, exercise.scope, exercise.definition, exercise.notes, exercise.rating, exercise.updated_time, exercise.practice_time],
+    ['INSERT INTO ', request.params['table'], ' (', columns.join(', '), ') VALUES (', placeholders.join(', '), ')'].join(''),
+    params,
     function (error) {
       if (error != null) {
         response.status(500).json(error);
@@ -42,15 +67,29 @@ app.post('/api/v1/exercises', function (request, response) {
   );
 });
 
-app.put('/api/v1/exercises/:id', function (request, response) {
+app.patch('/api/v1/:table/:id', function (request, response) {
+  console.log('PATCH ' + request.path);
   var exercise = request.body;
-  console.log(exercise);
+
+  var setters = [];
+  var params = [];
+  for (var column in exercise) {
+    setters.push(column + ' = ?');
+    params.push(exercise[column]);
+  }
+
+  params.push(request.params['id']);
+
+  var statement = ['UPDATE ', request.params['table'], ' SET ', setters.join(', '), ' WHERE id = ?'].join('');
+  console.log(statement);
+  console.log(params);
   db.run(
-    'UPDATE exercises SET scope = ?, definition = ?, notes = ?, rating = ?, updated_time = ?, practice_time = ? WHERE id = ?',
-    [exercise.scope, exercise.definition, exercise.notes, exercise.rating, exercise.updated_time, exercise.practice_time, request.params.id],
+    statement,
+    params,
     function (error) {
       debugger;
       if (error != null) {
+        console.log(error);
         response.status(500).json(error);
       } else {
         response.json({'changes': this.changes});
