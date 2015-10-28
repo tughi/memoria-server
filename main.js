1c1113e53d64ff0761f586d2cd556788f4b972f8
Tughi <admin@tughi.com>
2015-10-28 22:31:02 +0100

    unique scope_letters

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 var fs = require('fs');
 
-var sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('content.db');
 
 db.get('PRAGMA user_version', [], function (error, row) {
   switch (row.user_version) {
     case 0:
-      var exercises = JSON.parse(fs.readFileSync('memoria-exercises.json', 'utf8'));
+      var exercises = JSON.parse(fs.readFileSync('../memoria-exercises.json', 'utf8'));
 
       db.serialize(function () {
-        db.run('CREATE TABLE exercises (id INTEGER PRIMARY KEY, created_time INTEGER NOT NULL, updated_time INTEGER NOT NULL, scope TEXT NOT NULL UNIQUE, definition TEXT NOT NULL, notes TEXT, rating INTEGER NOT NULL, practice_time INTEGER NOT NULL)');
+        db.run('CREATE TABLE exercises (id INTEGER PRIMARY KEY, created_time INTEGER NOT NULL, updated_time INTEGER NOT NULL, scope TEXT NOT NULL, scope_letters TEXT NOT NULL UNIQUE, definition TEXT NOT NULL, notes TEXT, rating INTEGER NOT NULL, practice_time INTEGER NOT NULL)');
         db.run('PRAGMA user_version = 1');
 
         var updatedTime = new Date().getTime();
         for (var index in exercises) {
           var exercise = exercises[index];
-          console.log(exercise);
           db.run(
-            'INSERT INTO exercises (scope, definition, notes, rating, practice_time, created_time, updated_time) VALUES (?, ?, ?, ?, ?, ?, ?)',
-            [exercise.scope, exercise.definition, exercise.notes, exercise.rating, exercise.practiceTime * 1000, exercise.id, updatedTime]
+            'INSERT INTO exercises (scope, scope_letters, definition, notes, rating, practice_time, created_time, updated_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [exercise.scope, exercise.scopeLetters, exercise.definition, exercise.notes, exercise.rating, exercise.practiceTime, exercise.id, updatedTime],
+            function (error) {
+              if (error) {
+                console.log(error);
+              }
+            }
           )
         }
       });
@@ -34,14 +38,14 @@ var app = express();
 
 app.use('/api', bodyParser.json());
 
-app.get('/api/v1/:table', function (request, response) {
+app.get('/api/v1/exercises', function (request, response) {
   console.log('GET ' + request.path);
-  db.all(['SELECT * FROM ', request.params['table'], ' ORDER BY id'].join(''), function (error, rows) {
+  db.all('SELECT * FROM exercises ORDER BY id', function (error, rows) {
     response.json(rows);
   })
 });
 
-app.post('/api/v1/:table', function (request, response) {
+app.post('/api/v1/exercises', function (request, response) {
   console.log('POST ' + request.path);
   var row = request.body;
 
@@ -55,7 +59,7 @@ app.post('/api/v1/:table', function (request, response) {
   }
 
   db.run(
-    ['INSERT INTO ', request.params['table'], ' (', columns.join(', '), ') VALUES (', placeholders.join(', '), ')'].join(''),
+    ['INSERT INTO exercises (', columns.join(', '), ') VALUES (', placeholders.join(', '), ')'].join(''),
     params,
     function (error) {
       if (error != null) {
@@ -67,7 +71,7 @@ app.post('/api/v1/:table', function (request, response) {
   );
 });
 
-app.patch('/api/v1/:table/:id', function (request, response) {
+app.patch('/api/v1/exercises/:id', function (request, response) {
   console.log('PATCH ' + request.path);
   var exercise = request.body;
 
@@ -80,7 +84,7 @@ app.patch('/api/v1/:table/:id', function (request, response) {
 
   params.push(request.params['id']);
 
-  var statement = ['UPDATE ', request.params['table'], ' SET ', setters.join(', '), ' WHERE id = ?'].join('');
+  var statement = ['UPDATE exercises SET ', setters.join(', '), ' WHERE id = ?'].join('');
   console.log(statement);
   console.log(params);
   db.run(
